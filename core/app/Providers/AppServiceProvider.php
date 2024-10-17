03c71382965f93663b26ce1df4bbc85826775891
mjinternaldev <mj.internaldevteam@gmail.com>
2024-10-17 22:27:29 +0800

    fix: done local setup

--- core/app/Providers/AppServiceProvider.php
+++ core/app/Providers/AppServiceProvider.php
@@ -17,7 +17,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-		\URL::forceScheme('https');
+		// \URL::forceScheme('https'); // TODO: remove
         Schema::defaultStringLength(191);
         $gnl = General::first();
 
