1072960f4b2ec37702f1fad35a1e312a8567821e
Edcel Estadola <edcelestadola@Edcels-MacBook-Pro.local>
2024-12-02 14:45:11 +0800

    jpdte

--- agwiki-blayzer/agwiki-blayzer/core/resources/views/layouts/auth.blade.php
+++ agwiki-blayzer/agwiki-blayzer/core/resources/views/layouts/auth.blade.php
@@ -1531,7 +1531,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
                     $('.search-area-mobile').css('display', 'none');
                     $(this).attr('id', 'get_search-form')
                 })
-                toastr.success("Post Published Successfully.")
+                // toastr.success("Post Published Successfully.")
             });
             $(document).on('click', '.imgclickcls', function() {
                 var image = $(this).attr('src');
